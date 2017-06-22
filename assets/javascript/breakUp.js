1f303417ee3cf990e60d9de4d7352d8609a55cb3
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-06-21 19:00:10 -0600

    added the firebase config

---
+++ assets/javascript/breakUp.js
@@ -0,0 +1,14 @@
+// Add the Firebase Database
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBOzSjqz7LpVFYpVO5McXdXVq4O7T1Q1No",
+    authDomain: "gogopowerrangers-2632a.firebaseapp.com",
+    databaseURL: "https://gogopowerrangers-2632a.firebaseio.com",
+    projectId: "gogopowerrangers-2632a",
+    storageBucket: "",
+    messagingSenderId: "317199683141"
+  };
+  firebase.initializeApp(config);
+
+// Add the Spotify API
+
